35cb804c6c7bd2016103f4367d5d2cfd3e4c79eb
WebsiteSOLA <otabek_qoldoshev133@mail.ru>
2022-02-12 09:50:27 +0500

    added burger + navbar commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".navbar");
 
 burger.addEventListener("click", function() {
   burger.classList.toggle("header__burger--opened");
+  nav.classList.toggle("navbar--opened")
 })
