3ecb181c8ec85fb0b3bea382def9cac90bbe5093
WebsiteSOLA <otabek_qoldoshev133@mail.ru>
2022-02-15 13:13:56 +0500

    added about faqs true commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,20 @@ const nav = document.querySelector(".navbar");
 
 burger.addEventListener("click", function() {
   burger.classList.toggle("header__burger--opened");
-  nav.classList.toggle("navbar--opened")
+  nav.classList.toggle("navbar--opened");
 })
+const work = document.querySelector(".work");
+const workDesc = document.querySelector(".question__item-desc");
+
+work.addEventListener("click", function() {
+  work.classList.toggle("work--opened");
+  workDesc.classList.toggle("question__item-desc--opened");
+})
+const safe = document.querySelector(".safe");
+const safeDesc = document.querySelector(".question__item-desc");
+
+safe.addEventListener("click", function() {
+  safe.classList.toggle("safe--opened");
+  safeDesc.classList.toggle("question__item-desc--opened");
+})
+
